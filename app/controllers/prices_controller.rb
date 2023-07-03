1e3f8a45c137a7c34adb9e1a0ae40f0db433ae6d
junoota0115 <j1u1n5@gmail.com>
2023-07-03 18:21:54 +0900

    ログインしていない場合はログイン画面へ遷移

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,5 +1,12 @@
 class PricesController < ApplicationController
+    before_action :move_to_signed_in
+
     def index
+        @prices = Price.where(user_id: current_user.id)
+        @total_price = 0
+        @prices.each do |price|
+        @total_price += price.price
+    end
     end
     
     def new 
@@ -28,5 +35,12 @@ class PricesController < ApplicationController
     private
     def price_params
         params.require(:price).permit(:price,:content).merge(user_id: current_user.id)
+    end
+
+    def move_to_signed_in
+        unless user_signed_in?
+          #サインインしていないユーザーはログインページが表示される
+          redirect_to  '/users/sign_in'
+        end
     end
 end
